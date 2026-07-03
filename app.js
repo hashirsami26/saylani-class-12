94ec40802d2781a4a9824e816243f04d6f94dddb
hashirsami26 <hashirsami26@gmail.com>
2026-07-03 17:11:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+// premetive data type 
+// non premetive data type
+
+//                              premetive data type 
+// string
+// number
+// boolean
+// null
+//  undefined
+
+
+//                              non premetive data type
+
+// array 
+// object
+
+
+// let arr = []
+
+// let student = ['wasif', ' waleed', 'arshad', 'waqas', ' ali'];
+// let std =['ali', 123, true , null , undefined]
+
+// document.write(student);
+
+
+// push (array ke end mai add)
+//  pop (array ke end mai ek value delete karna)
+//  shift (array ke start se ek value delete kart hai )
+// unshift (array ke start mai element add karna)
+
+
+// let student = ['wasif', ' waleed', 'arshad', 'waqas', ' ali'];
+
+// console.log(student);
+// student.push('hussain');
+
+// console.log(student);
+
+let student = ['wasif', ' waleed', 'arshad', 'waqas', ' ali'];
+
+// console.log(student);
+
+// student.pop()
+
+// console.log(student);
+
+// student.shift()
+
+// console.log(student);
+
+console.log (student);
+
+student.unshift(1234)
+
+console.log(student)
