3e15d70f779a0eeb3defa1c8b3da281937554098
hashirsami26 <hashirsami26@gmail.com>
2026-07-03 17:33:10 +0500

    Add files via upload

---
+++ APP12.js
@@ -0,0 +1,76 @@
+// premetive data type 
+// non premetive data type
+
+//                              premetive data type 
+// string
+// number
+// boolean
+// null
+//  undefined
+
+
+//                              non premetive data type
+
+// array 
+// object
+
+
+// let arr = []
+
+// let student = ['wasif', ' waleed', 'arshad', 'waqas', ' ali'];
+// let std =['ali', 123, true , null , undefined]
+
+// document.write(student);
+
+
+// push (array ke end mai add)
+//  pop (array ke end mai ek value delete karna)
+//  shift (array ke start se ek value delete kart hai )
+// unshift (array ke start mai element add karna)
+// slice ( kuch elements copy karne deta hai)
+// splice (array mai kahi bhi add ya delet kar sakta hai)
+
+// let student = ['wasif', ' waleed', 'arshad', 'waqas', ' ali'];
+
+// console.log(student);
+// student.push('hussain');
+
+// console.log(student);
+
+// let student = ['wasif', ' waleed', 'arshad', 'waqas', ' ali'];
+
+// console.log(student);
+
+// student.pop()
+
+// console.log(student);
+
+// student.shift()
+
+// console.log(student);
+
+// console.log (student);
+
+// student.unshift(1234)
+
+// console.log(student)
+
+
+// let student = ['wasif', ' waleed', 'arshad', 'waqas', ' ali'];
+
+
+// let copy = student.slice(0, 2);
+// let copy2= student.slice(2, 4);
+
+// console.log (student);
+// console.log('copy1',copy);
+// console.log('copy2', copy2);
+
+
+// let student = ['wasif', ' waleed', 'arshad', 'waqas', ' ali'];
+
+// console.log('before', student);
+
+// student.splice(0, 0, 'MAJID');
+
+// console.log('after', student);
